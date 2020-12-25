f6ed9aa4768475159c43908d40ea5977a81a1aa5
Yury Vinogradov <yuvinogradov@gmail.com>
2020-12-25 09:52:09 +0200

    moved to redux

---
+++ src/redux/redux-store.js
@@ -0,0 +1,15 @@
+import {combineReducers, createStore} from "redux";
+import sidebarReducer from "./sidebar-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer
+})
+
+let store = createStore(reducers);
+
+
+export default store;

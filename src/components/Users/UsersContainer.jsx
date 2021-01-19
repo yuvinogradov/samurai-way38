450306c4576cca3b7fd73eea67edde5cb9709ad6
Yury Vinogradov <yuvinogradov@gmail.com>
2021-01-19 05:02:26 +0200

    56

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,8 +2,47 @@ import React from 'react';
 //import UsersF from "./UsersF";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import * as axios from "axios";
 import Users from "./Users";
 
+
+class UsersContainerComponent extends React.Component {
+
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChanged = (pageNumber) => {  //Димыч: мы договорились, что будем писать методы как стрелочные функции,
+        // чтобы сохранить контекст вызова
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+
+            });
+
+    }
+
+
+    render() {   //определяем метод рендер
+
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}
+
+
+        />
+    }
+}
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
@@ -12,7 +51,6 @@ let mapStateToProps = (state) => {
         currentPage: state.usersPage.currentPage,
     }
 }
-
 let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId)=> {
@@ -35,4 +73,4 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(Users);
+export default connect(mapStateToProps,mapDispatchToProps)(UsersContainerComponent);

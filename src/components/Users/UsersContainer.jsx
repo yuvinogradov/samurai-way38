03834f1a5dfa6551dec5aa6aa7c943601c98b25d
Yury Vinogradov <yuvinogradov@gmail.com>
2021-01-19 05:48:14 +0200

    57

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 //import UsersF from "./UsersF";
 import {connect} from "react-redux";
-import {followAC, setCurrentPageAC, setUsersTotalCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {
+    followAC,
+    setCurrentPageAC,
+    setUsersTotalCountAC,
+    setUsersAC,
+    unfollowAC,
+    toggleIsFetchingAC
+} from "../../redux/users-reducer";
 import * as axios from "axios";
 import Users from "./Users";
+import preloader from './../../assets/images/tail-spin.svg'
+import Preloader from "../common/preloader/Preloader";
 
 
 class UsersContainerComponent extends React.Component {
 
 
     componentDidMount() {
+        this.props.toggleIsFetching(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
+                this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items)
                 this.props.setTotalUsersCount(response.data.totalCount)
             });
@@ -20,8 +31,11 @@ class UsersContainerComponent extends React.Component {
     onPageChanged = (pageNumber) => {  //Димыч: мы договорились, что будем писать методы как стрелочные функции,
         // чтобы сохранить контекст вызова
         this.props.setCurrentPage(pageNumber)
+        this.props.toggleIsFetching(true)
+
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
+                this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items)
 
             });
@@ -31,29 +45,34 @@ class UsersContainerComponent extends React.Component {
 
     render() {   //определяем метод рендер
 
-        return <Users totalUsersCount={this.props.totalUsersCount}
-                      pageSize={this.props.pageSize}
-                      currentPage={this.props.currentPage}
-                      onPageChanged={this.onPageChanged}
-                      users={this.props.users}
-                      follow={this.props.follow}
-                      unfollow={this.props.unfollow}
+        return <>
+            {this.props.isFetching ? <Preloader/> : null}
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   onPageChanged={this.onPageChanged}
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}
 
 
-        />
+            />
+        </>
     }
 }
+
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching,
     }
 }
 let mapDispatchToProps = (dispatch) => {
     return {
-        follow: (userId)=> {
+        follow: (userId) => {
             dispatch(followAC(userId));
         },
         unfollow: (userId) => {
@@ -69,8 +88,11 @@ let mapDispatchToProps = (dispatch) => {
             dispatch(setUsersTotalCountAC(totalCount))
 
         },
+        toggleIsFetching: (isFetching) => {
+            dispatch(toggleIsFetchingAC(isFetching))
+        },
     }
 }
 
 
-export default connect(mapStateToProps,mapDispatchToProps)(UsersContainerComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainerComponent);
---
+++ src/components/common/preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "../../../assets/images/tail-spin.svg";
+import React from 'react'
+
+let Preloader = (props) => {
+    return <img src={preloader}/>
+}
+
+export default Preloader

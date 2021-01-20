12c1ffa5746b3f36cf73e167c7005785bafcf227
Yury Vinogradov <yuvinogradov@gmail.com>
2021-01-20 05:56:42 +0200

    60. withRouter. getting params from url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
                 <Route path='/dialogs'
                        render={() => <DialogsContainer/>}/>
 
-                <Route path='/profile'
+                <Route path='/profile/:userId?'  // ? - optional parameter
                        render={() => <ProfileContainer/>}/>
                 <Route path='/users'
                        render={() => <UsersContainer/>} />
